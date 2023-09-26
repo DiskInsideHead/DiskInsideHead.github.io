54d2af22a7dd80c518b752bbe92c5f85b6dd24e1
Disk Inside Head <138657329+DiskInsideHead@users.noreply.github.com>
2023-09-26 05:10:33 +0300

    Add files via upload

---
+++ precache-manifest.e0567ff02a04405e64784dfc29a2e37c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a609ec1126861881b82d",
+    "url": "/static/css/main.d9357f9a.chunk.css"
+  },
+  {
+    "revision": "a609ec1126861881b82d",
+    "url": "/static/js/main.a609ec11.chunk.js"
+  },
+  {
+    "revision": "97b0b8a42b79b1d85cc7",
+    "url": "/static/js/1.97b0b8a4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b0c4aab464e74e4b6a1df1c0771d298",
+    "url": "/index.html"
+  }
+];
